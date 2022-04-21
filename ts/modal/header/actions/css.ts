8d346769763ddb6b98cdbe87045429d642110de0
ctl2 <icallumlatham@gmail.com>
2022-04-21 14:09:26 +0100

    Made border between active header buttons visible

--- ts/modal/header/actions/css.ts
+++ ts/modal/header/actions/css.ts
@@ -11,26 +11,29 @@ export function addColourRule(actionId: string, colour: string) {
 }
 
 export default function generate() {
-    addRule([
-        `.${BUTTON_CLASS}:focus > svg`, `.${BUTTON_CLASS}:hover > svg`,
-        `.${BUTTON_CLASS}.${BUTTON_ACTIVE_CLASS} > svg`
-    ], [
-        ['background-color', `var(--headContrast)`]
-    ]);
-
-    addRule([
-        `.${BUTTON_CLASS}:focus:not(.${BUTTON_ACTIVE_CLASS}) > svg`,
-        `.${BUTTON_CLASS}:hover:not(.${BUTTON_ACTIVE_CLASS}) > svg`
-    ], [
-        ['stroke', `var(--headBase)`]
-    ]);
-
     addRule(`#${BUTTON_CONTAINER_ID}`, [
         ['display', 'inline-flex'],
         ['flex-direction', 'row']
     ]);
 
+    addRule([
+        `.${BUTTON_CLASS}:focus > svg`,
+        `.${BUTTON_CLASS}:hover > svg`,
+        `.${BUTTON_CLASS}.${BUTTON_ACTIVE_CLASS} > svg`
+    ], ['background-color', `var(--headContrast)`]);
+
     addRule(`.${BUTTON_CLASS}`, ['border-left', '2px solid var(--headContrast)']);
+
+    addRule([
+        `.${BUTTON_CLASS}:not(:first-child):focus`,
+        `.${BUTTON_CLASS}:not(:first-child):hover`,
+        `.${BUTTON_CLASS}:not(:first-child).${BUTTON_ACTIVE_CLASS}`
+    ], ['border-color', 'var(--headBase)']);
+
+    addRule([
+        `.${BUTTON_CLASS}:focus:not(.${BUTTON_ACTIVE_CLASS}) > svg`,
+        `.${BUTTON_CLASS}:hover:not(.${BUTTON_ACTIVE_CLASS}) > svg`
+    ], ['stroke', `var(--headBase)`]);
 
     addRule(`.${BUTTON_CLASS} > svg`, [
         ['width', '1.81em'],
