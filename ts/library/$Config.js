c41cfe85e53f71550d7c8b9e0f1245cffb6f0466
ctl2 <callumtylerlatham@gmail.com>
2025-01-10 11:55:40 +0000

    made `ready` a promise rather than a function & replaced TITLE arg with GM.info.script.name

--- ts/library/$Config.js
+++ ts/library/$Config.js
@@ -16,11 +16,11 @@ const STYLE_OUTER = {
 };
 
 export default class $Config {
-	constructor(TITLE, KEY_TREE, TREE_DEFAULT, STYLE_INNER = {}, _STYLE_OUTER = {}) {
+	constructor(KEY_TREE, TREE_DEFAULT, STYLE_INNER = {}, _STYLE_OUTER = {}) {
 		// PERMISSION CHECKS
 		
 		const getError = (reason, error) => {
-			const message = `[${TITLE}]${reason.includes('\n') ? '\n\n' : ' '}${reason}`;
+			const message = `[${GM.info.script.name}]${reason.includes('\n') ? '\n\n' : ' '}${reason}`;
 			
 			if (error) {
 				error.message = message;
@@ -45,200 +45,192 @@ export default class $Config {
 		
 		// PRIVATE
 		
-		let isOpen = false;
-		
 		const styleOuter = {
 			...STYLE_OUTER,
 			..._STYLE_OUTER,
 		};
 		
+		const target = (() => {
+			let targetWindow = window;
+			
+			while (targetWindow.frameElement) {
+				targetWindow = window.parent;
+			}
+			
+			const id = `${SOCKET_ID}-${KEY_TREE}`;
+			
+			for (const child of targetWindow.document.body.children) {
+				if (child.id === id) {
+					child.remove();
+					
+					break;
+				}
+			}
+			
+			const target = document.createElement('iframe');
+			
+			target.id = id;
+			
+			for (const [property, value] of Object.entries(styleOuter)) {
+				target.style[property] = value;
+			}
+			
+			target.style.display = 'none';
+			
+			targetWindow.document.body.appendChild(target);
+			
+			return target;
+		})();
+		
+		let isOpen = false;
+		
+		const open = (doOpen = true) => new Promise((resolve) => {
+			isOpen = doOpen;
+			
+			target.style.display = doOpen ? (styleOuter.display ?? 'initial') : 'none';
+			
+			// Delay script execution until visual update
+			setTimeout(resolve, 0);
+		});
+		
+		const disconnect = () => new Promise((resolve) => {
+			isOpen = false;
+			
+			target.remove();
+			
+			// Delay script execution until visual update
+			setTimeout(resolve, 0);
+		});
+		
 		// PUBLIC
 		
-		this.ready = async () => {
-			// Setup root element
-			const target = (() => {
-				let targetWindow = window;
-				
-				while (targetWindow.frameElement) {
-					targetWindow = window.parent;
-				}
-				
-				const id = `${SOCKET_ID}-${KEY_TREE}`;
-				
-				for (const child of targetWindow.document.body.children) {
-					if (child.id === id) {
-						child.remove();
-						
-						break;
-					}
-				}
-				
-				const target = document.createElement('iframe');
-				
-				target.id = id;
-				
-				for (const [property, value] of Object.entries(styleOuter)) {
-					target.style[property] = value;
-				}
-				
-				target.style.display = 'none';
-				
-				targetWindow.document.body.appendChild(target);
-				
-				return target;
-			})();
-			
+		this.ready = Promise.all([
+			GM.getValue(KEY_TREE),
+			GM.getValue(KEY_STYLES, []),
+			GM.getValue(KEY_VERSION, -1),
+		])
 			// Retrieve data
-			
-			const [userTree, userStyles, version] = await Promise.all([
-				GM.getValue(KEY_TREE),
-				GM.getValue(KEY_STYLES, []),
-				GM.getValue(KEY_VERSION, -1),
-			]);
-			
+			.then(([userTree, userStyles, version]) => {
 			// Patch to current version
-			
-			(() => {
-				if (!userTree) {
-					return;
-				}
 				
-				switch (version) {
-					case -1: {
-						const patch = (node) => {
-							delete node.predicate;
-							delete node.childPredicate;
-							delete node.descendantPredicate;
-							delete node.seed;
-							
-							if ('children' in node) {
-								for (const child of node.children) {
-									patch(child);
-								}
-							}
-						};
-						
-						patch(userTree);
+				(() => {
+					if (!userTree) {
+						return;
 					}
 					
-					// eslint-disable-next-line no-fallthrough
-					case 0: {
-						const patch = (node) => {
-							delete node.input;
-							
-							if ('children' in node) {
-								for (const child of node.children) {
-									patch(child);
+					switch (version) {
+						case -1: {
+							const patch = (node) => {
+								delete node.predicate;
+								delete node.childPredicate;
+								delete node.descendantPredicate;
+								delete node.seed;
+								
+								if ('children' in node) {
+									for (const child of node.children) {
+										patch(child);
+									}
 								}
-							}
-						};
+							};
+							
+							patch(userTree);
+						}
 						
-						patch(userTree);
+						// eslint-disable-next-line no-fallthrough
+						case 0: {
+							const patch = (node) => {
+								delete node.input;
+								
+								if ('children' in node) {
+									for (const child of node.children) {
+										patch(child);
+									}
+								}
+							};
+							
+							patch(userTree);
+						}
 					}
-				}
-			})();
-			
-			// Listen for post-init communication
-			
-			const open = (doOpen = true) => new Promise((resolve) => {
-				isOpen = doOpen;
+				})();
 				
-				target.style.display = doOpen ? (styleOuter.display ?? 'initial') : 'none';
-				
-				// Delay script execution until visual update
-				setTimeout(resolve, 0);
-			});
-			
-			const disconnect = () => new Promise((resolve) => {
-				isOpen = false;
-				
-				target.remove();
-				
-				// Delay script execution until visual update
-				setTimeout(resolve, 0);
-			});
-			
-			/**
+				/**
 			 * @name $Config#reset
 			 * @description Deletes the user's data.
 			 * @returns {Promise<void>} Resolves upon completing the deletion.
 			 */
-			this.reset = async () => {
-				if (isOpen) {
-					throw getError('Cannot reset while the UI is open.');
-				}
+				this.reset = async () => {
+					if (isOpen) {
+						throw getError('Cannot reset while the UI is open.');
+					}
+					
+					if (typeof GM.deleteValue !== 'function') {
+						throw getError('Missing GM.deleteValue permission.');
+					}
+					
+					await GM.deleteValue(KEY_TREE);
+					
+					// It may have previously been a rejected promise
+					this.ready = () => Promise.resolve();
+					
+					reset();
+				};
 				
-				if (typeof GM.deleteValue !== 'function') {
-					throw getError('Missing GM.deleteValue permission.');
-				}
-				
-				await GM.deleteValue(KEY_TREE);
-				
-				// It may have previously been a rejected promise
-				this.ready = () => Promise.resolve();
-				
-				reset();
-			};
-			
-			/**
+				/**
 			 * @name $Config#edit
 			 * @description Allows the user to edit the active config.
 			 * @returns {Promise<void>} Resolves when the user closes the config editor.
 			 */
-			this.edit = async () => {
-				if (isOpen) {
-					throw getError('A config editor is already open.');
-				}
-				
-				open();
-				
-				const {tree, config, styles} = await edit();
-				
-				GM.setValue(KEY_TREE, tree);
-				GM.setValue(KEY_STYLES, styles);
-				GM.setValue(KEY_VERSION, VERSION);
-				
-				this.get = ((config) => config).bind(null, Object.freeze(config));
-				
-				await open(false);
-			};
-			
-			// Pass data
-			
-			const response = await init({
-				userStyles,
-				defaultTree: TREE_DEFAULT,
-				title: TITLE,
-				defaultStyle: STYLE_INNER,
-				...(userTree ? {userTree} : {}),
-			}, target.contentDocument.body, target.contentWindow)
-				.catch(async (error) => {
-					delete this.reset;
+				this.edit = async () => {
+					if (isOpen) {
+						throw getError('A config editor is already open.');
+					}
 					
-					await disconnect();
+					open();
 					
-					this.ready = () => Promise.reject();
+					const {tree, config, styles} = await edit();
 					
-					throw getError(
-						'Your config is invalid.'
-						+ '\nThis could be due to a script update or your data being corrupted.'
-						+ `\n\nReason:\n${error.message.replaceAll(/\n+/g, '\n')}`,
-						error,
-					);
-				});
-			
-			this.get = ((config) => config).bind(null, Object.freeze(response.config));
-			
-			if (response.requireReset) {
+					GM.setValue(KEY_TREE, tree);
+					GM.setValue(KEY_STYLES, styles);
+					GM.setValue(KEY_VERSION, VERSION);
+					
+					this.get = ((config) => config).bind(null, Object.freeze(config));
+					
+					await open(false);
+				};
+				
+				// Pass data
+				
+				return init({
+					userStyles,
+					defaultTree: TREE_DEFAULT,
+					title: GM.info.script.name,
+					defaultStyle: STYLE_INNER,
+					...(userTree ? {userTree} : {}),
+				}, target.contentDocument.body, target.contentWindow);
+			})
+			.catch(async (error) => {
+				delete this.reset;
+				
+				await disconnect();
+				
 				throw getError(
 					'Your config is invalid.'
 					+ '\nThis could be due to a script update or your data being corrupted.'
-					+ `\n\nReason:\n${response.error.message.replaceAll(/\n+/g, '\n')}`,
-					response.error,
+					+ `\n\nReason:\n${error.message.replaceAll(/\n+/g, '\n')}`,
+					error,
 				);
-			}
-			
-			this.ready = () => Promise.resolve();
-		};
+			})
+			.then((response) => {
+				this.get = ((config) => config).bind(null, Object.freeze(response.config));
+				
+				if (response.requireReset) {
+					throw getError(
+						'Your config is invalid.'
+						+ '\nThis could be due to a script update or your data being corrupted.'
+						+ `\n\nReason:\n${response.error.message.replaceAll(/\n+/g, '\n')}`,
+						response.error,
+					);
+				}
+			});
 	}
 }
